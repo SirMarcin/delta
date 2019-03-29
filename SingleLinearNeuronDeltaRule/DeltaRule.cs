49e8eb842bbc8e1a03a21359e907e084231a42cc
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-03-29 14:36:14 +0100

    Refaktoryzacja

---
+++ SingleLinearNeuronDeltaRule/DeltaRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinearNeuronDeltaRule
+{
+    class DeltaRule
+    {
+        public int K { get; set; }
+        public int k { get; set; }
+        public double Step { get; set; }
+        public List<TrainingPattern> TrainingPatterns { get; set; }
+        public double[] Weights { get; set; }
+        public double Output { get; set; }
+        public int Nx { get; set; }
+    }
+}
---
+++ SingleLinearNeuronDeltaRule/TrainingPattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinearNeuronDeltaRule
+{
+    public class TrainingPattern
+    {
+        public TrainingPattern(int ny)
+        {
+            Ny = ny;
+            
+            Random randomNumber = new Random();
+
+            for (int i = 0; i < Ny; i++)
+            {
+                Inputs[i] = randomNumber.Next(-100000, 100000);
+            }
+
+            DesiredOutput = randomNumber.Next(-100000, 100000);
+        }
+
+        public int Ny { get; set; }
+        public double[] Inputs { get; set; }
+        public double DesiredOutput { get; set; }
+    }
+}
