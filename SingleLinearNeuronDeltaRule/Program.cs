56921599f3ee2c475bb4018f8ac3874a964cece0
Marcin Webmaster <buczynski.programuje@gmail.com>
2019-03-29 15:35:26 +0100

    to chyba nie tak

--- SingleLinearNeuronDeltaRule/Program.cs
+++ SingleLinearNeuronDeltaRule/Program.cs
@@ -10,6 +10,18 @@ namespace SingleLinearNeuronDeltaRule
     {
         static void Main(string[] args)
         {
+            Console.Write("Set a number of training patterns: ");
+            int numberOfTrainingPatterns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Set a size of training patterns: ");
+            int sizeOfTrainingPatterns = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Set a number of training epochs: ");
+            int numberOfTrainingEpochs = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Set a size of training step: ");
+            double sizeOfTrainingsteps = Convert.ToDouble(Console.ReadLine());
+
         }
     }
 }
